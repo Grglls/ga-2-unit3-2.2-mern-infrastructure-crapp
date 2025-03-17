973df35cfa33ba99e7509c6e341885f8cd16ecf4
Greg Ells <gregells@gmail.com>
2025-03-17 19:46:23 -0400

    Add api controller with create action

---
+++ controllers/api/users.js
@@ -0,0 +1,30 @@
+const jwt = require('jsonwebtoken');
+const User = require('../../models/user');
+
+module.exports = {
+  create,
+};
+
+async function create(req, res) {
+  try {
+    // Add the user to the database:
+    const user = await User.create(req.body);
+    // token will be a string:
+    const token = createJWT(user);
+    // Use res.json to send back just a string:
+    // (the client code needs to take this into consideration)
+    res.json(token);
+  } catch (error) {
+    res.status(400).json(error);
+  }
+}
+
+// Helper functions:
+function createJWT(user) {
+  return jwt.sign(
+    // Data payload:
+    { user },
+    process.env.SECRET,
+    { expiresIn: '24h' }
+  )
+}
